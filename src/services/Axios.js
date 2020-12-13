97fa1a9ce431df96940bf57cd1a79c8768e50e67
rohit <rohitchatla@gmail.com>
2020-12-13 10:36:40 +0530

    works added prod build

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,9 +1,12 @@
 import Axios from "axios";
 
 //const baseURL = "http://localhost:5000/api";
-const baseURL = "https://workify-services.herokuapp.com//api";
+//const assestsURL = "http://localhost:5000/assets/";
+
+const baseURL = "https://workify-services.herokuapp.com/api";
+const assestsURL = "https://workify-services.herokuapp.com/assets/";
 
 Axios.defaults.baseURL = baseURL;
 
 export default Axios;
-export { baseURL }; //assetsURL
+export { baseURL, assestsURL };
