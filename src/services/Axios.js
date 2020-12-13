8988f38b5e1a4676ce79cc54e23914cf2ba4bbe8
rohit <rohitchatla@gmail.com>
2020-12-13 13:16:56 +0530

    payment gateway integrated and routes done

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,10 +1,10 @@
 import Axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
-const assestsURL = "http://localhost:5000/assets/";
+// const baseURL = "http://localhost:5000/api";
+// const assestsURL = "http://localhost:5000/assets/";
 
-// const baseURL = "https://workify-services.herokuapp.com/api";
-// const assestsURL = "https://workify-services.herokuapp.com/assets/";
+const baseURL = "https://workify-services.herokuapp.com/api";
+const assestsURL = "https://workify-services.herokuapp.com/assets/";
 
 Axios.defaults.baseURL = baseURL;
 
